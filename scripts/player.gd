e7e5bef218313ae21d3f7e18887e048d2bd9d1ad
ObaniGemini <robin@pi-et-ro.net>
2018-08-23 16:06:15 +0200

    Rework physics, add hacking action and animation We need to somehow avoid the player sliding on slopes when not moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,35 +1,46 @@
 extends KinematicBody2D
 
-const SPEED = 200				# moving speed
-const JUMP_SPEED = 150			# jumping speed
-const GRAVITY = 350				# falling gravity
-
+const SPEED = 300				# moving speed
+const GRAVITY = 2000				# falling gravity
 var vel = Vector2(0, 0) # Stores movement for one physics step
+
 
 func _ready():
 	set_physics_process(true)
+	set_process_input(true)
+
 
 func _physics_process(delta):
 	
 	# Handle movement inputs
+	vel.x = 0
 	if Input.is_action_pressed("move_left"):
-		vel.x = -SPEED
-	elif Input.is_action_pressed("move_right"):
-		vel.x = SPEED
-	else:
-		vel.x = 0
+		vel.x -= SPEED
+	if Input.is_action_pressed("move_right"):
+		vel.x += SPEED
 	
-	#Handle jumping
+	# Handle jumping
 	if is_on_floor() && Input.is_action_pressed("jump"):
-		vel.y = -GRAVITY*0.5
+		vel.y = -GRAVITY*0.35
 	
-	#Handle falling, stop on the floor
+	# Handle falling, stop on the floor
 	if(!is_on_floor()):
 		vel.y += delta*GRAVITY
 	elif(vel.y > 0):
 		vel.y = 0
 	
-	#Perform the actual movement. Floor normal is into negative y direction.
-	#No infinite inertia, stop on slopes.
-	move_and_slide(vel, Vector2(0,-1), false, true)
-	
+	# Perform the actual movement. Floor normal is into negative y direction.
+	# No infinite inertia, stop on slopes.
+	move_and_slide(vel, Vector2(0,-1), false, false, 4)
+
+
+func _input(event):
+	if event.is_action_pressed("hack") and not get_node("hacking/hack_anim").is_playing():
+		hack()
+
+
+func hack():
+	get_node("hacking/hack_anim").play("hack")
+	for body in get_node("hacking").get_overlapping_bodies():
+		if body.is_in_group("hackable"):
+			print("taking control of "+body.get_name())
